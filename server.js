5b2b13cdcaeea76f69dd01f576bb836cbde1fd7c
ketan2035 <ketankumar147856@gmail.com>
2026-05-29 23:30:27 +0530

    fix2

--- server.js
+++ server.js
@@ -29,6 +29,4 @@ app.use("/api",professionalRoutes);
 
 // start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
